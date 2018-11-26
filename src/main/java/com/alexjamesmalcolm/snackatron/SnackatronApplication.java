435eceae9dda0993af274e0f7d1743b6a9065b98
Alexander James Malcolm <ajmalcolm@aep.com>
2018-11-26 14:27:15 -0500

    Initial commit

---
+++ src/main/java/com/alexjamesmalcolm/snackatron/SnackatronApplication.java
@@ -0,0 +1,12 @@
+package com.alexjamesmalcolm.snackatron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackatronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackatronApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alexjamesmalcolm/snackatron/SnackatronApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alexjamesmalcolm.snackatron;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SnackatronApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
