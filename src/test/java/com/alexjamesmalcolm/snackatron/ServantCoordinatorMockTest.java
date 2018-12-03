4486555d5dc05aaa957736c1267cf63b25f2ceff
Alexander James Malcolm <ajmalcolm@aep.com>
2018-12-03 17:49:42 -0500

    Renamed ServantCoordinatorTest to ServantCoordinatorMockTest

--- src/test/java/com/alexjamesmalcolm/snackatron/ServantCoordinatorMockTest.java
+++ src/test/java/com/alexjamesmalcolm/snackatron/ServantCoordinatorMockTest.java
@@ -1,12 +1,20 @@
 package com.alexjamesmalcolm.snackatron;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
 
 public class ServantCoordinatorTest {
@@ -31,5 +39,12 @@ public class ServantCoordinatorTest {
         MockitoAnnotations.initMocks(this);
         Iterable<Servant> servants = asList(jim, john, joey);
         when(servantRepo.findAll()).thenReturn(servants);
+    }
+
+    @Test
+    public void shouldRetrieveServantWhoHasNotServedRecently() {
+        when(servantRepo.findFirstByOrderByLastServiceAsc()).thenReturn(jim);
+        Servant actual = underTest.whoHasNotServedRecently();
+        assertThat(actual, is(jim));
     }
 }
